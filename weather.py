bb546f1982decb4d4eb2f43063c18006a1a2a15a
wesley <wesleypdavidson@gmail.com>
2018-10-06 19:30:54 -0700

    lets see if python works

---
+++ weather.py
@@ -0,0 +1,8 @@
+a = input("Is it raining?:")
+if a == "yes":
+    print("well then you need a rain jacket")
+b = input("Is it cold out?:")
+if b == "yes":
+    print("Then you need a coat")
+print("Then you need no specil wear")
+
